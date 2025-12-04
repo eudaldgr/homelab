6bbe77f2d02aa03f1c5af069642ed2e18dec8ab7
Eudald Gubert i Roldan <hola@eudald.gr>
2025-12-04 10:52:25 +0100

    feat(ansible): add secrets to container host + complete pangolin, forgejo stack

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
+HCLOUD_IP=$(tofu -chdir=../infra/hetzner/homelab output -raw server_ip)
+
+sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
+mv -f _ inventory/hosts.ini
+
 ANSIBLE_CONFIG=./ansible.cfg \
 ansible-playbook container-host.yml "$@"
