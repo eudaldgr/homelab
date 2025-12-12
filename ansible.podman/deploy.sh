02655d055b49be44aeb4e6a95b2bbb111dfaaf50
Eudald Gubert i Roldan <hola@eudald.gr>
2025-12-12 22:11:02 +0100

    fix(ansible): Change path on deploy script

--- ansible.podman/deploy.sh
+++ ansible.podman/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-HCLOUD_IP=$(tofu -chdir=../infra/hetzner/homelab output -raw server_ip)
+HCLOUD_IP=$(tofu -chdir=../tofu/hetzner/homelab output -raw server_ip)
 
 sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
 mv -f _ inventory/hosts.ini
