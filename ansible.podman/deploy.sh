9f6cb8a959d4fe753a218097090b49a378e0bf87
Eudald Gubert i Roldan <hola@eudald.gr>
2025-12-15 13:58:47 +0100

    feat(ansible): Upgrade podman containers host ansible roles

--- ansible.podman/deploy.sh
+++ ansible.podman/deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -e
 
-HCLOUD_IP=$(tofu -chdir=../tofu/hetzner/homelab output -raw server_ip)
-
-sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
-mv -f _ inventory/hosts.ini
-
 ANSIBLE_CONFIG=./ansible.cfg \
-ansible-playbook container-host.yml "$@"
+ansible-playbook containers-host.yaml "$@"
