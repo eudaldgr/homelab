775bafd69efc690339698b280599d190d5ae9e7f
Eudald Gubert i Roldan <hola@eudald.gr>
2026-01-08 15:15:04 +0100

    chore(ansible): Move pangolin deploy to ansible.podman

--- ansible.podman/deploy.sh
+++ ansible.podman/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
+# HCLOUD_IP=$(tofu -chdir=../tofu/hetzner/homelab output -raw server_ip)
+
+# sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
+# mv -f _ inventory/hosts.ini
+
 ANSIBLE_CONFIG=./ansible.cfg \
 ansible-playbook containers-host.yaml "$@"
