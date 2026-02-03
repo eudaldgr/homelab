417973fef3210f8fc479b6cb5a5b39f0548a4f7c
Eudald Gubert i Roldan <hola@eudald.gr>
2026-02-03 14:08:44 +0100

    refactor(ansible): replace podman rootless with --userns=auto

--- ansible.podman/roles.deprecated/podman-rootless/files/rootless.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck disable=SC2148
-
-XDG_RUNTIME_DIR="/run/user/$(id -u)"
-
-export XDG_RUNTIME_DIR
-export PODMAN_COMPOSE_WARNING_LOGS=false
-export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
