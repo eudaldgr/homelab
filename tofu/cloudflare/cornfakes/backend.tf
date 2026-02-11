a5007e7e4cbc7088eb180ec48be7d5a34b4754ce
Eudald Gubert i Roldan <hola@eudald.gr>
2026-02-11 19:54:12 +0100

    chore(tofu): remove deprecated S3 backend configuration and related files

--- tofu/cloudflare/cornfakes/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "e17n-homelab-tofu-state"
-    key          = "infra/cloudflare/cornfakes/terraform.tfstate"
-    region       = "eu-west-3"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
