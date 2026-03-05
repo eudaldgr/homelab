9455db8d4463ba6a77f7bf805020eef6f86f5666
Eudald Gubert i Roldan <hola@eudald.gr>
2026-03-05 15:01:38 +0100

    fix(tofu): add sealed-secrets namespace and update resource dependencies

--- tofu/home.arpa/fgs/pve/modules/k8s/secrets.tf
+++ tofu/home.arpa/fgs/pve/modules/k8s/secrets.tf
@@ -1,5 +1,12 @@
+resource "kubernetes_namespace_v1" "sealed_secrets" {
+  metadata {
+    name = "sealed-secrets"
+  }
+}
+
 resource "null_resource" "restore_sealed_secrets_master_key" {
-  count = var.restore_sealed_secrets_master_key ? 1 : 0
+  depends_on = [kubernetes_namespace_v1.sealed_secrets]
+  count      = var.restore_sealed_secrets_master_key ? 1 : 0
 
   provisioner "local-exec" {
     command = <<-EOT
