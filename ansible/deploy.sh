c2045a17487d0e7f80da1dcdae240e79834d5cbd
Eudald Gubert i Roldan <hola@eudald.gr>
2026-07-20 22:08:16 +0200

    chore: cleanup ansible path

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,14 +2,9 @@
 
 export ANSIBLE_CONFIG=./ansible.cfg
 
-if [ "$1" = "--pangolin" ]; then
-  HCLOUD_IP=$(tofu -chdir=../tofu/hetzner/homelab output -raw server_ip)
+HCLOUD_IP=$(tofu -chdir=../tofu/hetzner/homelab output -raw server_ip)
 
-  sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
-  mv -f _ inventory/hosts.ini
+sed "s/pangolin01 ansible_host=.*/pangolin01 ansible_host=${HCLOUD_IP}/" inventory/hosts.ini > _
+mv -f _ inventory/hosts.ini
 
-  shift
-  ansible-playbook pangolin.yaml --skip-tags sops,age "$@"
-else
-  ansible-playbook containers-host.yaml "$@"
-fi
+ansible-playbook pangolin.yaml --skip-tags sops,age "$@"
